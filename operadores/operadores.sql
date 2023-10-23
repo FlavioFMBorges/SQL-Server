913fcce51f5732802955783ff7753a356666e82a
Flávio Borges <76125191+FlavioFMBorges@users.noreply.github.com>
2023-10-23 14:08:45 -0300

    Operadores

---
+++ operadores/operadores.sql
@@ -0,0 +1,20 @@
+-- Aprendendo Operadores
+
+Operador	Descrição					Exemplo
+
++			Soma						2 + 2 = 4
+-			Subtração					3 - 2 = 1
+/			Divisão						4 / 2 = 2
+%			Módulo						5 % 2 = 1
+=			Igual						4 = 4 = V ou atribuição  colunaA = 'xyz'
+>			Maior que					3 > 2 = V
+<			Menor que					3 < 2 = F
+>=			Maior ou igual que			5 >= 4 = V
+<=			Menor ou igual que			5 <= 5 = V
+<>			Diferente de				Procure e mostre A diferente de B
+AND			Operador lógico e			Procure e mostre A e B
+OR			Operador lógico ou			Procure e mostre A ou B
+
+Procurar por Operador lógico e Operador Relacional em SQL Server
+
+https://learn.microsoft.com/pt-br/sql/t-sql/language-elements/arithmetic-operators-transact-sql?view=sql-server-ver16
